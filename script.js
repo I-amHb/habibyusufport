3dfed96ff9a4e21cd3af82e83b06031cd7e916b3
Habib <yusufhabib3@gmail.com>
2025-02-27 18:59:12 +0100

    backend and ops skill upload

--- script.js
+++ script.js
@@ -32,8 +32,12 @@ xMenu.addEventListener('click', closeNav);
 // Skills Display
 const skill = document.querySelector('.skills');
 const f_end = document.querySelector('.frontend');
+const stackHouse = document.querySelector('.stackHouse');
 const b_end = document.querySelector('.backend');
+const stackHouse1 = document.querySelector('.stackHouse1');
 const ops_ = document.querySelector('.ops');
+const stackHouse2 = document.querySelector('.stackHouse2');
+
 
 const frontEnd = [
     { name: 'HTML', filepath: './images/icons/HTML-icon.png' },
@@ -45,26 +49,73 @@ const frontEnd = [
 
 
 frontEnd.map((val) => {
+
+    let skillHouse = document.createElement('div');
+    skillHouse.setAttribute('class', 'divCont')
+
     let icon = document.createElement('img')
     icon.setAttribute('src', val.filepath);
     icon.setAttribute('class', 'icon');
-    let p = document.createElement('p');
-    p.textContent = val.name;
-    f_end.appendChild(icon);
-    f_end.appendChild(p);
-    console.log(p);
+
+    let title = document.createElement('p');
+    title.textContent = val.name;
+// Nests both p and icon inside  skillHouse
+    skillHouse.appendChild(icon);
+    skillHouse.appendChild(title);
+
+    stackHouse.appendChild(skillHouse);
+    console.log(skillHouse);
 })
 
 const backEnd = [
-    {name: 'Node.js', filepath: './images/icons/'},
-    {name: 'Express.js', filepath: './images/icons/'},
-    {name: 'Python', filepath: './images/icons/'},
+    {name: 'Node.js', filepath: './images/icons/node-js-icon.png'},
+    {name: 'Express.js', filepath: './images/icons/express-icon480.png'},
+    {name: 'Python', filepath: './images/icons/python-icon.png'},
 ];
+
+
+backEnd.map((val) => {
+
+    let skillHouse = document.createElement('div');
+    skillHouse.setAttribute('class', 'divCont')
+
+    let icon = document.createElement('img')
+    icon.setAttribute('src', val.filepath);
+    icon.setAttribute('class', 'icon');
+
+    let title = document.createElement('p');
+    title.textContent = val.name;
+// Nests both p and icon inside  skillHouse
+    skillHouse.appendChild(icon);
+    skillHouse.appendChild(title);
+
+    stackHouse1.appendChild(skillHouse);
+    console.log(skillHouse);
+})
 
 
 const ops = [
-    {name: 'Git', filepath: './images/icons/'},
-    {name: 'Vercel', filepath: './images/icons/'}
+    {name: 'Git', filepath: './images/icons/git-icon.png'},
+    {name: 'Vercel', filepath: './images/icons/vercel-icon.png'}
 ];
 
 
+
+ops.map((val) => {
+
+    let skillHouse = document.createElement('div');
+    skillHouse.setAttribute('class', 'divCont')
+
+    let icon = document.createElement('img')
+    icon.setAttribute('src', val.filepath);
+    icon.setAttribute('class', 'icon');
+
+    let title = document.createElement('p');
+    title.textContent = val.name;
+// Nests both p and icon inside  skillHouse
+    skillHouse.appendChild(icon);
+    skillHouse.appendChild(title);
+
+    stackHouse2.appendChild(skillHouse);
+    console.log(skillHouse);
+})
